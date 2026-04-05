f0d1c2264b987bc6b5b0247a60eaad82efc7b536
AngellaMirembe <angellamirembe96@gmail.com>
2026-04-06 00:36:03 +0300

    added js functionality

---
+++ functionality.js
@@ -0,0 +1,38 @@
+const form = document.getElementById("teamForm");
+
+form.addEventListener("submit", function(event){
+  event.preventDefault(); //prevent page reload
+  console.log("Form submitted");
+
+//get values
+const teamName = document.getElementById("teamName").value;
+const speaker1 = document.getElementById("speaker1").value;
+const speaker2 = document.getElementById("speaker2").value;
+const speaker3 = document.getElementById("speaker3").value;
+
+//get selected genders
+const gender1 = document.querySelector('input[name="gender1"]:checked')?.value;
+const gender2 = document.querySelector('input[name="gender2"]:checked')?.value;
+const gender3 = document.querySelector('input[name="gender3"]:checked')?.value;
+
+//create object
+const teamData = {
+  teamName,
+  speakers: [
+    {name: speaker1, gender: gender1},
+    {name: speaker2, gender: gender2},
+    {name: speaker3, gender: gender3}
+  ]
+};
+
+console.log(teamData);
+
+//store temporarily
+let allTeams = JSON.parse(localStorage.getItem("teams"))||[];
+allTeams.push(teamData);
+localStorage.setItem("teams", JSON.stringify(allTeams));
+
+//reset form
+form.reset();
+
+});
